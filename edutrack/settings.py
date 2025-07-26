d246e6e5a39e1cfb77c22db38fb30bf233976bcd
Natanael Hernandez <natanaelhdz9@gmail.com>
2025-07-25 18:54:26 -0600

    feat: debug test4

--- edutrack/settings.py
+++ edutrack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'RENDER' not in os.environ
-DEBUG = os.environ.get("DEBUG_FORCE") == "true"
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
