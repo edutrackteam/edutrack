73d41a2e718b67f470802d05ac82170f94da840a
Natanael Hernandez <natanaelhdz9@gmail.com>
2025-07-25 18:17:27 -0600

    feat: debug test2

--- edutrack/settings.py
+++ edutrack/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'RENDER' not in os.environ
-DEBUG = os.environ.get("DEBUG_FORCE") == "true"
+DEBUG = 'RENDER' not in os.environ
+#DEBUG = os.environ.get("DEBUG_FORCE") == "true"
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
