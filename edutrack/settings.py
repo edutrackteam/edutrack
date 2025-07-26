24fc314218758ba8f8f66b54fe0251698df5d091
Natanael Hernandez <natanaelhdz9@gmail.com>
2025-07-25 18:56:51 -0600

    feat: debug testcuatro

--- edutrack/settings.py
+++ edutrack/settings.py
@@ -128,9 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
